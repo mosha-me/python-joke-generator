6b77527d2b519ec8d066bf8d3f7f9a5769678357
MOSHA ME <mdmaruf.wai.ai.4565@gmail.com>
2026-06-25 17:23:45 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+import random
+
+jokes = [
+    "Why did the math book look sad? - Because it had too many problems!",
+    "Why do programmers prefer dark mode? - Because light attracts bugs!", 
+    "Why was the computer cold? - Because it left its Windows open!",
+    "Why did the developer go broke? - Because he used up all his cache!",
+    "Why is Python so cool? - Because it's snake-tastic!",
+    "What did the router say to the doctor? - I need a bandwidth-aid!"
+    "Why don't programmers like nature? - It has too many bugs!"
+    ]
+    
+print(random.choice(jokes))
+
